5fc28f744220df27d120b0d53984341c74b5356e
dora orak <doraorak@gmail.com>
2025-01-23 23:54:24 +0300

    turn into header file. remove example. effectively making this a single header library, and the example is moved to "example" branch

---
+++ hookInstance/hookInstance.h
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  hookInstance
+//
+//  Created by Dora Orak on 22.01.2025.
+//
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
+#import <AppKit/AppKit.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+
+void hookInstance(id instance, SEL targetSEL, IMP replacementFp, IMP* origFp) {
+    
+    char* replacementClassName = [[NSString stringWithFormat:@"%@_hook_", [instance class]] stringByAppendingString:[[NSUUID UUID] UUIDString]].cString;
+    
+    Class replacementClass = objc_allocateClassPair([instance class], replacementClassName, 0);
+    objc_registerClassPair(replacementClass);
+    
+    Method origMethod = class_getInstanceMethod([instance class], targetSEL);
+        
+    *origFp = method_getImplementation(origMethod);
+    
+    char* typenc = method_getTypeEncoding(origMethod);
+        
+    class_replaceMethod(replacementClass, targetSEL, replacementFp, typenc);
+    
+    object_setClass(instance, replacementClass);
+}
