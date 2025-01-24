2d07df73134d4fce4de480298ef82b29be6f18c9
dora orak <doraorak@gmail.com>
2025-01-24 22:48:52 +0300

    make hooked objects return base class in -[class]

--- hookInstance.h
+++ hookInstance.h
@@ -4,18 +4,31 @@
 //
 //  Created by Dora Orak on 22.01.2025.
 //
+
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
+Class clsHook(id self, SEL _cmd){
+
+    NSMutableString* className = [NSString stringWithCString:class_getName(object_getClass(self))];
+    NSMutableString* baseClassName = className;
+    
+    if([className containsString:@"_instanceHook_"]){
+        baseClassName = [NSMutableString stringWithString:[[className componentsSeparatedByString:@"_instanceHook_"] objectAtIndex:0]];
+    }
+    
+    return objc_getClass(baseClassName.cString);
+
+}
 
 void hookInstance(id instance, SEL targetSEL, IMP replacementFp, IMP* origFp) {
     
-    char* replacementClassName = [[NSString stringWithFormat:@"%@_hook_", [instance class]] stringByAppendingString:[[NSUUID UUID] UUIDString]].cString;
+    char* replacementClassName = [[NSString stringWithFormat:@"%@_instanceHook_", object_getClass(instance)] stringByAppendingString:[[NSUUID UUID] UUIDString]].cString;
     
-    Class replacementClass = objc_allocateClassPair([instance class], replacementClassName, 0);
+    Class replacementClass = objc_allocateClassPair(object_getClass(instance), replacementClassName, 0);
     objc_registerClassPair(replacementClass);
     
-    Method origMethod = class_getInstanceMethod([instance class], targetSEL);
+    Method origMethod = class_getInstanceMethod(object_getClass(instance), targetSEL);
     
     if(origFp != NULL)
     *origFp = method_getImplementation(origMethod);
@@ -23,6 +36,10 @@ void hookInstance(id instance, SEL targetSEL, IMP replacementFp, IMP* origFp) {
     const char* typenc = method_getTypeEncoding(origMethod);
         
     class_replaceMethod(replacementClass, targetSEL, replacementFp, typenc);
-    
+
+    //lie about the class
+    class_replaceMethod(replacementClass, @selector(class), (IMP)clsHook, "#16@0:8");
+ 
+    //apply hook
     object_setClass(instance, replacementClass);
 }
